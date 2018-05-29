4078ef0a9bf2658068a8d0fd3bbdb6286d1e0270
John Stewart <johnstewartco@gmail.com>
2018-05-29 08:57:08 -0600

    basic page setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Task from './Task';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <Task/>
+    </div>,
+    destination
+);
